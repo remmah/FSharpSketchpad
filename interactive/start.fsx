d3b3a38312d79132b52487cc0bb835c9bbf78b3a
remmah <source@remmah.net>
2019-04-03 23:34:40 -0400

    Added part 2 of F# syntax

--- interactive/start.fsx
+++ interactive/start.fsx
@@ -33,3 +33,40 @@ let simplePatternMatch =
     | "b" -> printfn "x is b"
     | _ -> printfn "x is something else"
 
+let validValue = Some(99)
+let invalidValue = None
+
+let optionPatternMatch input =
+    match input with
+        | Some i -> printfn "input is an int=%d" i
+        | None -> printfn "input is missing"
+
+optionPatternMatch validValue
+optionPatternMatch invalidValue
+
+// Tuples
+let twoTuple = 1,2
+let threeTuple = "a",2,true
+
+// Record types
+type Person = {First:string; Last:string}
+let person1 = {First="john"; Last="Doe"}
+
+// Union types are for choices
+type Temp =
+    | DegreesC of float
+    | DegreesF of float
+let temp = DegreesF 98.6
+
+// Combining types recursively
+type Employee =
+    | Worker of Person
+    | Manager of Employee list
+let jdoe = {First="John";Last="Doe"}
+let worker = Worker jdoe
+
+// Printing
+printfn "Printing an int %i, a float %f, a bool %b" 1 2.0 true
+printfn "A string %s, and something generic %A" "hello" [1;2;3;4]
+
+printfn "twoTuple=%A,\nPerson=%A,\nTemp=%A,\nEmployee=%A" twoTuple person1 temp worker
