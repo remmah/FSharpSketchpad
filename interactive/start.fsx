ec013cd5059215ec061b9aa529789e2c1ed320e3
remmah <source@remmah.net>
2019-04-05 23:48:28 -0400

    Page 5 webpage downloader

--- interactive/start.fsx
+++ interactive/start.fsx
@@ -77,3 +77,25 @@ let sumOfSquares n =
     [1..n]
     |> List.map square
     |> List.sum
+
+// Page 5
+open System.Net
+open System
+open System.IO
+
+// Fetch webpage contents
+let fetchUrl callback url = 
+    let req = WebRequest.Create(Uri(url))
+    use resp = req.GetResponse() //'use' lets us dispose IDisposable when out of scope
+    use stream = resp.GetResponseStream()
+    use reader = new IO.StreamReader(stream)
+    callback reader url
+
+let myCallback (reader:IO.StreamReader) url =
+    let html = reader.ReadToEnd()
+    let html1000 = html.Substring(0,1000)
+    printfn "Downloaded %s. First 1000 is %s" url html1000
+    html // returns all the html
+
+// test
+let google = fetchUrl myCallback "https://google.com"
